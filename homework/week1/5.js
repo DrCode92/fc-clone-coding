87d5e8450ed12b74ae322db2e617ff75fd45ca95
DrCode92 <dr.code92@gmail.com>
2021-04-08 11:34:37 +0900

    first commit

---
+++ homework/week1/5.js
@@ -0,0 +1,7 @@
+for (var i = 2; i <= 9; i++) {
+    // 아래를 완성하세요.
+    for (var j = 1; j <= 9; j++) {
+      console.log(`${i} x ${j} = ${i * j}`);
+    }
+    console.log("\n") * 3;
+  }
---
+++ lecture/nodejs/hello.js
@@ -0,0 +1,5 @@
+var a = "hello";
+var b = 123;
+var c = 4;
+
+console.log(b+c);
