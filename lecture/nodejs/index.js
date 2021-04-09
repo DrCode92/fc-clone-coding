c59866f2d643d105dcd8ee81b8a58c47cfe194fe
DrCode92 <dr.code92@gmail.com>
2021-04-09 17:26:33 +0900

    20210409

---
+++ lecture/nodejs/index.js
@@ -0,0 +1,9 @@
+// 불러올땐
+// require 파일명
+
+const myvar = require('./myvar');
+console.log(myvar.a);
+
+// const Myvar = require('./myvar');
+// const setVar = new Myvar();
+// console.log(setVar.name);
---
+++ lecture/nodejs/myvar.js
@@ -0,0 +1,11 @@
+// 내보낼땐
+// Module.exports 변수
+
+// const a = "hello a";
+module.exports.a = "hello a";
+
+// function Myvar() {
+//     this.name = "my instance"
+//     this.hello = "my instance hello"
+// }
+// module.exports = Myvar;
